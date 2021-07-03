2bc46aa1afcf91950457ff4532b161f78f029e38
Promise <glelud@gmail.com>
2021-07-03 02:19:29 +0200

    Added random inspiration

--- README_template.md
+++ README_template.md
@@ -11,3 +11,9 @@ a {{AGE}}-year old old node developer cruising through life with a terrible musi
 ## Countr Stats
 
 [Countr](https://countr.xyz/) is an an advanced counting bot which can manage a counting channel in your Discord guild and it is currently in {{COUNTR-SERVERS}} servers and is ranked [#{{COUNTR-RANKING}} on Discord](https://dblstatistics.com/bot/467377486141980682). We have successfully counted {{COUNTR-COUNTS}} counts this week!
+
+## Hourly dose of Inspiration
+
+![inspirobot.me]({{INSPIROBOT}})
+
+> Thanks for checking out my profile.
--- index.js
+++ index.js
@@ -7,6 +7,7 @@ fs.readFile("./README_template.md", "utf8", async (err, template) => {
   if (err) throw err;
 
   const countr = await fetch("https://countr-splash-stats.promise.workers.dev/").then(res => res.json()).catch(() => ({ server_count: "?", server_count_rank: "?", count: "?" }));
+  const inspirobot = await fetch("https://inspirobot.me/api?generate=true").then(res => res.text()).catch(() => "");
 
   fs.writeFile("./README.md", template
     .replace(/{{AGE}}/g, `\`${Math.floor((Date.now() - age) / yearInMs * 1e4) / 1e4}\``)
@@ -14,6 +15,8 @@ fs.readFile("./README_template.md", "utf8", async (err, template) => {
     .replace(/{{COUNTR-SERVERS}}/g, countr.server_count.toLocaleString("en-US"))
     .replace(/{{COUNTR-RANKING}}/g, countr.server_count_rank.toLocaleString("en-US"))
     .replace(/{{COUNTR-COUNTS}}/g, countr.count.toLocaleString("en-US"))
+
+    .replace(/{{INSPIROBOT}}/g, inspirobot)
 
   , "utf8", () => console.log("done"))
 })
