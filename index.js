596767bcbf29c5e97a120cac000fdd06e49d74f3
Glenn <glenn@not-tech.support>
2021-12-17 18:00:14 +0100

    Update index.js

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const fs = require("fs"), fetch = require("node-fetch");
 const age = 1075762800000;
 const yearInMs = 3.1556926e10;
 
-fs.readFile("./README_template.md", "utf8", async (err, template) => {
+fs.readFile("./README.md", "utf8", async (err, template) => {
   if (err) throw err;
 
   const countr = await fetch("https://countr-splash-stats.promise.workers.dev/").then(res => res.json()).catch(() => ({ server_count: "?", server_count_rank: "?", count: "?" }));
