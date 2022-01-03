2634720dddfd8f505a7c99903872e7e54dbf4ce8
Glenn <glenn@not-tech.support>
2022-01-03 21:08:37 +0100

    Update

--- src/widgets/activity.ts
+++ src/widgets/activity.ts
@@ -6,15 +6,15 @@ import emojis from "../utils/emojis";
 export type Event = Awaited<ReturnType<typeof octokit.activity.listEventsForAuthenticatedUser>>["data"][number];
 
 // they really fucked up the types on this one, had to make my own just to satisfy TypeScript
-const eventList: Record<string, (event: Event & any) => string> = { // eslint-disable-line @typescript-eslint/no-explicit-any
+const eventList: Record<string, (event: Event & any) => string | null> = { // eslint-disable-line @typescript-eslint/no-explicit-any
   IssueCommentEvent: (event: Event) => `${emojis.comment} Commented on #${event.payload.issue?.number} in ${event.repo.name}`,
   IssuesEvent: (event: Event) => `${emojis.issue} ${capitalize(event.payload.action as string)} issue #${event.payload.issue?.number} in ${event.repo.name}`,
   PullRequestEvent: (event: Event & { payload: { pull_request: { number: number; merged_at?: string } }}) => `${event.payload.pull_request?.merged_at ? `${emojis.prMerged} Merged` : `${event.payload.action === "opened" ? emojis.pr : emojis.prRejected} ${capitalize(event.payload.action as string)}`} PR #${event.payload.pull_request?.number} in ${event.repo.name}`,
   PullRequestReviewEvent: (event: Event & { payload: { pull_request: { number: number; }; review: { state: string; } }}) => `${emojis.review} ${capitalize(event.payload.action as string)} review (${event.payload.review.state}) on PR #${event.payload.pull_request?.number} in ${event.repo.name}`,
   ForkEvent: (event: Event & { payload: { forkee: { full_name: string; }}}) => `${emojis.fork} Forked ${event.payload.forkee.full_name} from ${event.repo.name}`,
   PushEvent: (event: Event & { payload: { size: number; }}) => `${emojis.push} Pushed ${event.payload.size} commit${event.payload.size > 1 ? "s" : ""} to ${event.repo.name}`,
-  CreateEvent: (event: Event) => `${emojis.create} Created ${event.repo.name}`,
-  DeleteEvent: (event: Event) => `${emojis.delete} Deleted ${event.repo.name}`,
+  CreateEvent: (event: Event & { payload: { ref_type: string; }}) => event.payload.ref_type === "repository" ? `${emojis.create} Created ${event.repo.name}` : null,
+  DeleteEvent: (event: Event & { payload: { ref_type: string; }}) => event.payload.ref_type === "repository" ? `${emojis.delete} Deleted ${event.repo.name}` : null,
 };
 
 export async function activity(rows = 25) {
@@ -23,5 +23,6 @@ export async function activity(rows = 25) {
     .filter(event => event.type && event.type in eventList)
     .slice(0, rows)
     .map(event => eventList[event.type as string](event))
+    .filter(Boolean)
     .join("\n");
 }
