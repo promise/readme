02e40aec744c3264eb0784c86b0cb7768b28af01
Glenn <glenn@not-tech.support>
2022-07-11 21:55:57 +0200

    Fix linting issues

--- src/index.ts
+++ src/index.ts
@@ -5,11 +5,8 @@ import { readFile, writeFile } from "fs/promises";
 import { capitalize } from "./utils/human";
 import { join } from "path";
 
-// github has really shitty types including one being { [key: string]: unknown; } which sucks massive donkey balls
-type User = Awaited<ReturnType<typeof octokit.users.getAuthenticated>>["data"];
-
 void readFile(join(__dirname, "../src/template.md"), "utf8").then(async template => {
-  const user = await octokit.users.getByUsername({ username }).then(({ data }) => data) as User;
+  const user = await octokit.users.getByUsername({ username }).then(({ data }) => data);
   const file = template
     .replace(/<!--NAME-->/gmu, user.name ?? capitalize(user.login))
     .replace(/<!--AGE-->/gmu, String(Math.round((Date.now() - ageSinceEpoch) / yearInMs * 10_000) / 10_000))
