7e04dee7cfe0e8a4d820830a16416b37130e3ff3
Glenn <glenn@not-tech.support>
2023-07-14 07:09:18 +0000

    fix: linting
    Signed-off-by: Glenn <glenn@not-tech.support>

--- src/utils/time.ts
+++ src/utils/time.ts
@@ -1,2 +1,2 @@
-// eslint-disable-next-line import/prefer-default-export -- multiple exports can be defined in this file
+// eslint-disable-next-line i/prefer-default-export -- multiple exports can be defined in this file
 export const yearInMs = 1000 * 60 * 60 * 24 * 365.2425;
