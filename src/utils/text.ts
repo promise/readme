f41e2240b70ea7eb57c0f2522ae35d31475220b6
Glenn <glenn@not-tech.support>
2024-12-10 11:57:37 +0000

    feat: node22 and new eslint config
    Signed-off-by: Glenn <glenn@not-tech.support>

--- src/utils/text.ts
+++ src/utils/text.ts
@@ -1,4 +1,4 @@
-// eslint-disable-next-line i/prefer-default-export -- multiple exports can be defined in this file
+// eslint-disable-next-line import/prefer-default-export -- multiple exports can be defined in this file
 export function capitalize(str: string): string {
   return str.charAt(0).toUpperCase() + str.slice(1);
 }
